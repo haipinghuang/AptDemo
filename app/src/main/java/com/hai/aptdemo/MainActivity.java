bfb62eab0b6e7cd0852d11ab007d52b71bcdecae
huanghp <huanghp@dztec.net>
2018-07-13 15:55:07 +0800

    apt顺利生成class；apt demo 完结

--- app/src/main/java/com/hai/aptdemo/MainActivity.java
+++ app/src/main/java/com/hai/aptdemo/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Inject.inject(this);
+//        Inject.inject(this);
     }
 
     public void clk(View view) {
--- compile/src/main/java/com/hai/apt/processor/NewClassProcessor.java
+++ compile/src/main/java/com/hai/apt/processor/NewClassProcessor.java
@@ -9,7 +9,9 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
@@ -25,7 +27,7 @@ import javax.tools.JavaFileObject;
  * create by huanghp at 2018/6/20
  * email 1132760021@qq.com
  */
-@AutoService(Process.class)
+@AutoService(Processor.class)//此处注意，别写成了Process，不然此类不会执行
 public class NewClassProcessor extends AbstractProcessor {
     private static final String TAG = "NewClassProcessor";
     private String packageName;
@@ -34,14 +36,9 @@ public class NewClassProcessor extends AbstractProcessor {
     @Override
     public synchronized void init(ProcessingEnvironment processingEnvironment) {
         super.init(processingEnvironment);
+        error("process begin init");
     }
 
-    @Override
-    public Set<String> getSupportedAnnotationTypes() {
-        Set<String> set = new LinkedHashSet<>();
-        set.add(NewClass.class.getCanonicalName());
-        return set;
-    }
 
     @Override
     public SourceVersion getSupportedSourceVersion() {
@@ -50,20 +47,21 @@ public class NewClassProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
-        error("process begin");
+        error("process begin process");
         Set<? extends Element> elementsAnnotatedWith = roundEnvironment.getElementsAnnotatedWith(NewClass.class);
         for (Element element : elementsAnnotatedWith) {
             if (element.getKind() == ElementKind.CLASS) {
-                ExecutableElement executableElement = (ExecutableElement) element;
-                TypeElement classElement = (TypeElement) executableElement.getEnclosingElement();
+                error(element.toString());
+//                ExecutableElement executableElement = (ExecutableElement) element;
+                TypeElement classElement = (TypeElement) element;
                 //full class name
                 String qfClassName = classElement.getQualifiedName().toString();
                 String className = classElement.getSimpleName().toString();
                 PackageElement packageElement = processingEnv.getElementUtils().getPackageOf(classElement);
                 //package name
-                String packageName = packageName = packageElement.getQualifiedName().toString();
-                System.out.println("className=" + qfClassName);
-                System.out.println("packageName=" + packageName);
+                packageName = packageElement.getQualifiedName().toString();
+                error("className=" + qfClassName);
+                error("packageName=" + packageName);
 
                 try {
                     JavaFileObject sourceFile = processingEnv.getFiler().createSourceFile(packageName + "." + className + "$$" + PROXY, classElement);
@@ -81,6 +79,14 @@ public class NewClassProcessor extends AbstractProcessor {
         return true;
     }
 
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        error("process begin getSupportedAnnotationTypes");
+        Set<String> set = new LinkedHashSet<>();
+        set.add(NewClass.class.getCanonicalName());
+        return set;
+    }
+
     public String getJavaCode(String proxyName) {
         StringBuilder builder = new StringBuilder();
         builder.append("// Generated code. Do not modify!\n");
@@ -96,12 +102,12 @@ public class NewClassProcessor extends AbstractProcessor {
 
     private void generateMethod(StringBuilder builder) {
         builder.append("public String getString(){\n");
-        builder.append("return this is zhe msg returned;");
+        builder.append("return \"this is zhe msg returned\";");
         builder.append("}\n");
     }
 
 
     private void error(String msg) {
-        processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, msg);
+        processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, msg);
     }
 }
